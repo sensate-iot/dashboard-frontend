03372c9c637eb22aed5b8c86705a0c441e849e0d
Michel Megens <michel@michelmegens.net>
2021-07-25 11:32:41 +0200

    Update dependencies
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,32 +6,24 @@
  */
 
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {LoginService} from './login.service';
 import {Measurement} from '../models/measurement.model';
 import {OrderDirection} from '../dto/orderdirection';
+import {Observable} from "rxjs/internal/Observable";
+import {Response} from "../dto/response";
+import {map} from "rxjs/operators";
 
 @Injectable()
 export class DataService {
-  private readonly options: {
-    headers?: HttpHeaders | {
-      [header: string]: string | string[];
-    };
-    observe?: 'body';
-    params?: HttpParams | {
-      [param: string]: string | string[];
-    };
-    reportProgress?: boolean;
-    responseType?: 'json';
-    withCredentials?: boolean;
-  };
 
-  public constructor(
-    private readonly http: HttpClient,
-    private readonly login: LoginService
-  ) {
-    this.options = {};
+  private static transformResponse<TValue>(response: Observable<Response<TValue>>): Observable<TValue> {
+    return response.pipe(map((response, idx) => {
+      return response.data;
+    }));
+  }
+
+  public constructor(private readonly http: HttpClient) {
   }
 
   public get(sensorId: string, start: Date, end: Date, limit: number = 0, skip: number = 0, order: OrderDirection = OrderDirection.none) {
@@ -49,6 +41,7 @@ export class DataService {
       url += `&order=${order}`;
     }
 
-    return this.http.get<Measurement[]>(url, this.options);
+    const resp = this.http.get<Response<Measurement[]>>(url);
+    return DataService.transformResponse(resp);
   }
 }
